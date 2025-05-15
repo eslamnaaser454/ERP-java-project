d7ad6200bcc5ef99c58bc8bef44093b9a856e87c
Omar-lashin <omaramrabdelkaderlashin@gmail.com>
2025-05-15 03:17:00 +0300

    Making folder for download uml photos

--- src/main/java/app/Stores/StoreFactories/Store.java
+++
@@ -1,9 +0,0 @@
-package app.Stores.StoreFactories;
-
-public interface Store {
-    String getId();
-    String getName();
-    String getLocation();
-    void saveToDatabase();
-    void deleteFromDatabase();
-}
